418e8b9720d65cb533af8b99476c4467d636584a
paledust <paledust@hotmail.com>
2017-02-20 16:16:22 -0500

    Map added
    Add map, Now, player can select the level on the overview map, and then move into the detail map for more information.
    The control right now is just right click and left click. Need to modified to be like mobile control.
    
    All the scripts have been put inside the Camera code,

---
+++ Assets/Scripts/Camera/CS_CameraControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_CameraControl : MonoBehaviour {
+
+	public Texture2D mouseTex;
+	public LayerMask rayCastLayer;
+	public float followSpeed;
+	public float closeDistance;
+
+	private Ray ray;
+	private RaycastHit rayhit;
+	private Vector3 originalPosition;
+	private Vector3 targetPos;
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor(mouseTex, Vector2.one * mouseTex.width/2, CursorMode.Auto);
+		originalPosition = transform.position;
+		targetPos = originalPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CameraSelect();
+		CameraMove(targetPos);
+	}
+
+	//Guide The Camera
+	void CameraMove(Vector3 _targetPosition)
+	{
+		transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * followSpeed);
+		if((transform.position - _targetPosition).magnitude <= 0.01f)
+			transform.position = _targetPosition;
+	}
+
+	//Cast ray to select the area
+	void CameraSelect()
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray.origin,ray.direction, out rayhit, 300.0f,rayCastLayer))
+			{
+				targetPos = new Vector3(rayhit.collider.transform.position.x, rayhit.collider.transform.position.y, closeDistance);
+				Debug.Log("Hit");
+			}
+		}
+
+		if(Input.GetButtonDown("Fire2"))
+		{
+			targetPos = originalPosition;
+		}
+	}
+
+	public float getDistanceRatio()
+	{
+		return (closeDistance - transform.position.z)/(closeDistance - originalPosition.z);
+	}
+}
---
+++ Assets/Scripts/Camera/CS_Level_Fade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Level_Fade : MonoBehaviour {
+
+	public SpriteRenderer map_Detail;
+	public SpriteRenderer map_Rough;
+
+	private SpriteRenderer currentMap;
+	// Use this for initialization
+	void Start () {
+		map_Rough.color = Color.white;
+		map_Detail.color = new Color(1,1,1,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float alpha = 0;
+
+		alpha = GetComponent<CS_CameraControl>().getDistanceRatio();
+		map_Detail.color = new Color(1,1,1,1-alpha);
+		map_Rough.color = new Color(1,1,1,alpha);
+	}
+}
