31a855b728b6b00b1a5ed2d3828f77dc1bc3f1db
Hang <whitewield@126.com>
2017-02-20 12:02:59 -0500

    Test Movement

---
+++ Assets/Scripts/CS_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Player : MonoBehaviour {
+	[SerializeField] float mySpeed = 10;
+
+	private GameObject myTarget;
+	private bool isMoving;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += Vector3.forward * mySpeed * Time.deltaTime * Input.GetAxis ("Vertical");
+	}
+}
