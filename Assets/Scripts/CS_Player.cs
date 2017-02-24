04c0307bf2e2cc6ee50678cd089818f070d7371f
Hang <whitewield@126.com>
2017-02-23 19:49:18 -0500

    Player eat food and move

--- Assets/Scripts/CS_Player.cs
+++ Assets/Scripts/CS_Player.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CS_Player : MonoBehaviour {
 	[SerializeField] float mySpeed = 10;
+	[SerializeField] float myDeltaDistance = 1;
 
-	private GameObject myTarget;
-	private bool isMoving;
+	private Vector3 myTargetPosition;
+	private bool isMoving = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +17,24 @@ public class CS_Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.transform.position += Vector3.forward * mySpeed * Time.deltaTime * Input.GetAxis ("Vertical");
+
+		if (isMoving) {
+			this.transform.position = Vector3.Lerp (
+				this.transform.position, myTargetPosition, Time.deltaTime * mySpeed * Time.deltaTime
+			);
+			if (Vector3.Distance (this.transform.position, myTargetPosition) < myDeltaDistance) {
+				isMoving = false;
+			}
+		} else if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast (ray, out hit, 100.0F);
+			if (hit.collider.tag == "Food") {
+				myTargetPosition = hit.transform.position;
+				isMoving = true;
+			}
+		}
 	}
+
+
 }
